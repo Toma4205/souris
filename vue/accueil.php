87ca23d5133ff73159642aef7ce30d87c587dff6
Max4409 <32123160+Max4409@users.noreply.github.com>
2017-10-06 10:53:13 +0200

    MP : Ajout contrôle présence coach dans souris.php

--- vue/accueil.php
+++ vue/accueil.php
@@ -2,6 +2,8 @@
 // entete
 require_once("vue/commun/entete.php");
 ?>
+    <p>Nombre de coachs : <?= $manager->count() ?></p>
+
     <form action="" method="post">
       <h1>Déjà inscrit ?</h1>
       <p>Nom coach : <input type="text" name="nom" size="40" value="<?php
