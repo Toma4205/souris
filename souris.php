83711ca308dae9e9de249321a42f13099a06c984
Max4409 <32123160+Max4409@users.noreply.github.com>
2017-10-06 10:48:05 +0200

    MP : Ajout contrôle présence coach dans souris.php

--- souris.php
+++ souris.php
@@ -3,15 +3,27 @@ require('controleur/autoLoad.php');
 require_once('modele/connexionSQL.php');
 require_once('controleur/constantesSession.php');
 
+session_start();
+
 if (!isset($_GET['section']) OR $_GET['section'] == 'index')
 {
     require_once('controleur/accueil/ctr_accueil.php');
 }
-elseif ($_GET['section'] == 'compteCoach')
+elseif (isset($_SESSION[ConstantesSession::COACH]))
 {
-    require_once('controleur/compteCoach/ctr_compteCoach.php');
+    $coach = $_SESSION[ConstantesSession::COACH];
+        
+    if ($_GET['section'] == 'compteCoach')
+    {
+        require_once('controleur/compteCoach/ctr_compteCoach.php');
+    }
+    elseif ($_GET['section'] == 'gestionAmis')
+    {
+        require_once('controleur/gestionAmi/ctr_gestionAmis.php');
+    }
 }
-elseif ($_GET['section'] == 'gestionAmis')
+else 
 {
-    require_once('controleur/gestionAmi/ctr_gestionAmis.php');
+    header('Location: souris.php');
+    exit();
 }
