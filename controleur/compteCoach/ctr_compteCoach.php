8e54cd5fa513b3889f6e3c6224bf36ea65f3ce86
Max4409 <32123160+Max4409@users.noreply.github.com>
2017-10-06 10:50:28 +0200

    MP : Ajout contrôle présence coach dans souris.php

--- controleur/compteCoach/ctr_compteCoach.php
+++ controleur/compteCoach/ctr_compteCoach.php
@@ -1,23 +1,14 @@
 <?php
 
-session_start();
-
-if (isset($_SESSION[ConstantesSession::COACH]))
+if (isset($_POST['creerLigue']))
 {
-  if (isset($_POST['creerLigue'])) {
     header('Location: souris.php?section=creationLigue');
-  }
-  elseif (isset($_POST['gererAmis'])) {
-    header('Location: souris.php?section=gestionAmis');
-  }
-  $coach = $_SESSION[ConstantesSession::COACH];
 }
-else {
-  header('Location: souris.php');
-  exit();
+elseif (isset($_POST['gererAmis'])) 
+{
+    header('Location: souris.php?section=gestionAmis');
 }
 
-$managerCoach = new CoachManager($bdd);
 $managerAmi = new AmiManager($bdd);
 
 $amis = $managerAmi->findAmisByIdCoach($coach->id());
