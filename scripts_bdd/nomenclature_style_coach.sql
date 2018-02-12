f580d2117b945f112e41df5a512d4103f731f7d1
Max4409 <plaisancemax@yahoo.fr>
2018-02-12 22:56:24 +0100

    MP : prise en compte style coach bielsa

--- scripts_bdd/nomenclature_style_coach.sql
+++ scripts_bdd/nomenclature_style_coach.sql
@@ -1,6 +1,6 @@
 INSERT INTO `nomenclature_style_coach`(`code`, `libelle`, `description`, `nom_image`, `date_debut`) VALUES
 ('CATENACCIO','Catenaccio', 'Si seulement on pouvait mettre 2 gardiens.', 'coach_ecole_italienne.png' ,'2017-01-01'),
-('DEGLINGO','Le déglingo', 'Quand tu préfères que les scores des matchs ressemblent à des sets de tennis.', 'coach_ecole_italienne.png' ,'2017-01-01'),
+('DEGLINGO','Le déglingo', 'Quand tu préfères que les scores des matchs ressemblent à des sets de tennis.', 'coach_bielsa.png' ,'2017-01-01'),
 ('FOOT_TOTAL','Football total', 'Tout le monde attaque, tout le monde défend.', 'coach_ecole_italienne.png' ,'2017-01-01'),
 ('TIKI_TAKA','Tiki-Taka', 'Quand tu crois que faire 300 passes éqjivaut à 1 but.', 'coach_ecole_italienne.png' ,'2017-01-01'),
 ('GUEULARD','Le gueulard', '...', 'coach_ecole_italienne.png' ,'2017-01-01'),
