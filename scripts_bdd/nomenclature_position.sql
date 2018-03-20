fc67ecc9269b33db3a4f191311353301563a64b9
Max4409 <32123160+Max4409@users.noreply.github.com>
2018-03-20 08:56:26 +0100

    MP : ajout nomenclature position

---
+++ scripts_bdd/nomenclature_position.sql
@@ -0,0 +1,13 @@
+INSERT INTO `nomenclature_position`(`code`, `libelle`, `date_debut`) 
+VALUES ('B','Buteur', '2017-01-01'),
+('AD','Ailier droit', '2017-01-01'),
+('AG','Ailier gauche', '2017-01-01'),
+('MO','Milieu offensif', '2017-01-01'),
+('MG','Milieu gauche', '2017-01-01'),
+('MD','Milieu droit', '2017-01-01'),
+('MC','Milieu centre', '2017-01-01'),
+('MDC','Milieu défensif', '2017-01-01'),
+('DG','Latéral gauche', '2017-01-01'),
+('DD','Latéral droit', '2017-01-01'),
+('DC','Défenseur central', '2017-01-01'),
+('G','Gardien', '2017-01-01');
