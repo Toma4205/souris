a6d041ffb1b453b899739d1bb57d4ac8c56dcd76
Max4409 <32123160+Max4409@users.noreply.github.com>
2018-03-21 14:12:51 +0100

    MP : correction maj_joueur_reel.php

--- maj_joueur_reel.php
+++ maj_joueur_reel.php
@@ -17,21 +17,40 @@ catch (Exception $e)
 	addLogEvent($e);
 }
 
-function findComplet()
+$nbElemParPage = 100;
+
+function compterComplet()
+{
+    global $bdd;
+	
+	$q = $bdd->prepare('SELECT count(j.id)
+		FROM joueur_reel j
+		WHERE j.anniv IS NOT NULL AND j.nationalite IS NOT NULL AND j.position_expert IS NOT NULL');
+    $q->execute();
+
+	return $q->fetchColumn();
+}
+
+function findComplet($page)
 {
     $joueurs = [];
 
 	global $bdd;
-	$q = $bdd->prepare('SELECT j.*, n.libelle as libelleEquipe 
+	global $nbElemParPage;
+	
+	$q = $bdd->prepare('SELECT j.id, j.nom, j.prenom, j.position, j.position_expert, j.position_secondaire, n.libelle as libelleEquipe, count(je.id_equipe) as nbCC 
 		FROM joueur_reel j
-        JOIN nomenclature_equipe n ON j.equipe = n.code 
+		JOIN nomenclature_equipe n ON j.equipe = n.code 
+		LEFT JOIN joueur_equipe je ON je.id_joueur_reel = j.id
 		WHERE j.anniv IS NOT NULL AND j.nationalite IS NOT NULL AND j.position_expert IS NOT NULL
-		ORDER BY j.nom, j.prenom');
+		GROUP BY j.id, j.nom, j.prenom, j.position, j.position_expert, j.position_secondaire, libelleEquipe
+		ORDER BY j.nom, j.prenom
+		LIMIT ' . $nbElemParPage . ' OFFSET ' . (($page - 1) * $nbElemParPage));
     $q->execute();
 
 	while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
 	{
-		$joueurs[] = new JoueurReel($donnees);
+		$joueurs[] = ['joueur' => new JoueurReel($donnees), 'nbCC' => $donnees['nbCC']];
 	}
 
 	$q->closeCursor();
@@ -273,7 +292,7 @@ function afficherJoueursIncomplets($joueursIncomplets, $nat, $pos)
 function afficherJoueursComplets($joueurs, $cat, $pos)
 {
 	if (sizeof($joueurs) > 0)
-	{	echo sizeof($joueurs) . ' joueur(s) complet(s). <br/><br/>';
+	{	
 		echo '<table border="1">';
 		echo '<thead>';
 		echo '<tr>';
@@ -290,13 +309,10 @@ function afficherJoueursComplets($joueurs, $cat, $pos)
 		echo '</thead>';
 		echo '<tbody>';
 
-		foreach($joueurs as $value)
+		foreach($joueurs as $joueur)
 		{
-			$nbEquipe = compterPresenceDansEquipe($value->id());
-			$anniv = '';
-			if ($value->anniv() != null) {
-				$anniv = date_format(date_create($value->anniv()), 'd/m/Y');
-			}
+			$value = $joueur['joueur'];
+			$nbEquipe = $joueur['nbCC'];
 			
 			echo '<tr><td>' . $value->id() . '</td>';
 			echo '<td>' . $value->nom() . ' ' . $value->prenom() . '</td>';
@@ -306,18 +322,13 @@ function afficherJoueursComplets($joueurs, $cat, $pos)
 			echo '<td>' . afficherSelectPositionSecondaire($pos, $value->id(), $value->positionSecondaire()) . '</td>';
 			echo '<td>' . $nbEquipe . '</td>';
 			echo '<td><input type="submit" value="Maj" name="maj_complet[' . $value->id() . ']" 
-				onclick="return confirm(\'Mettre à jour ' . $value->nom() . ' ' . $value->prenom() . '(' . $value->libelleEquipe() . ')' . '\');" /></td>';
+				onclick="return confirm(\'' . $value->id() . '- Mettre à jour ' . $value->nom() . ' ' . $value->prenom() . ' (' . $value->libelleEquipe() . ')' . '\');" /></td>';
 			echo '<td><input type="submit" value="Supprimer" name="supp[' . $value->id() . ']" 
-				onclick="return confirm(\'Supprimer ' . $value->nom() . ' ' . $value->prenom() . '(' . $value->libelleEquipe() . ')' . '\');" /></td></tr>';
+				onclick="return confirm(\'' . $value->id() . '- Supprimer ' . $value->nom() . ' ' . $value->prenom() . '(' . $value->libelleEquipe() . ')' . '\');" /></td></tr>';
 		}
     
 		echo '</tbody>';
 		echo '</table>';
-	}
-	else
-	{
-		echo '<br/>';
-		echo 'Aucun joueur complet !';
 	}
 }
 
@@ -353,7 +364,6 @@ elseif (isset($_POST['maj_complet']))
 		if ($_POST['pos_second_' . $cle] != -1) {
 			$posSecond = $_POST['pos_second_' . $cle];
 		}
-		
 		majJoueurReelComplet($cle, $_POST['cat_' . $cle], $_POST['pos_expert_' . $cle], $posSecond);
 	}
 }
@@ -361,7 +371,14 @@ elseif (isset($_POST['maj_complet']))
 $nat = findNationalite();
 $cat = findCategorie();
 $pos = findPosition();
-$joueursComplets = findComplet();
+
+$pageComplet = 1;
+if (isset($_POST['page_complet'])) {
+	$pageComplet = $_POST['page_complet'];
+}
+
+$totalComplets = compterComplet();
+$joueursComplets = findComplet($pageComplet);
 $joueursIncomplets = findIncomplet();
 
 ?>
@@ -373,8 +390,25 @@ $joueursIncomplets = findIncomplet();
 			<?php afficherJoueursIncomplets($joueursIncomplets, $nat, $pos); ?>
 		</form>
 		<br/>
-		<h2>Joueurs avec infos incomplètes</h2>
-		<form method="post" action="">
+		<h2>Joueurs avec infos complètes</h2>
+		<form id="formComplet" method="post" action="">
+			<div><?php echo $totalComplets ?> joueur(s) complet(s).</div>
+			<br/>
+			<div>Page : 
+				<select name="page_complet" onchange="javascript:submit();">
+				<?php
+					$max = ceil($totalComplets / $nbElemParPage);
+					for ($i = 1; $i <= $max; $i++)
+					{
+						if ($i == $pageComplet) {
+							echo '<option value="' . $i . '" selected="selected">' . $i . '</option>';
+						} else {
+							echo '<option value="' . $i . '">' . $i . '</option>';
+						}
+					}
+				?>
+				</select>
+			</div>
 			<?php afficherJoueursComplets($joueursComplets, $cat, $pos); ?>
 		</form>
 	</body>
