4a120a0f2d68e4e8bc1c0c2f10c4a47833653fcb
Max4409 <32123160+Max4409@users.noreply.github.com>
2018-03-22 11:36:33 +0100

    MP : ajout caricatureEnum.php

---
+++ modele/nomenclature/caricatureEnum.php
@@ -0,0 +1,19 @@
+<?php
+// Répertorie les codes nomenclature_caricature
+class CaricatureEnum {
+    const SOURIS = 'SOURIS';
+    const CHAMPION = 'CHAMPION';
+    const VICTIME = 'VICTIME';
+    const PEPITE = 'PEPITE';
+    const PIGEON = 'PIGEON';
+    const AUC_TROPHEE = 'AUC_TROPHEE';
+    const PIRE_ATTAQUE = 'PIRE_ATTAQUE';
+    const TONTON_PAT = 'TONTON_PAT';
+    const DEPENSIER = 'DEPENSIER';
+	const ECONOME = 'ECONOME';
+	const VOYANT = 'VOYANT';
+	const PIGNON = 'PIGNON';
+    const BUT_REEL_ENC = 'BUT_REEL_ENC';
+    const BUT_VIRTUEL_ENC = 'BUT_VIRTUEL_ENC';
+}
+ ?>
